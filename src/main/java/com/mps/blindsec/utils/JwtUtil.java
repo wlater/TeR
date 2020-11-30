28b66914b8570db3cb77f6cac1cfd3043f68e5d9
Erick <39500821+wlater@users.noreply.github.com>
2020-11-30 18:27:37 -0300

    ignore

--- src/main/java/com/mps/blindsec/utils/JwtUtil.java
+++ src/main/java/com/mps/blindsec/utils/JwtUtil.java
@@ -7,7 +7,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
